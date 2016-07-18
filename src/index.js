0ca328c7aa31e3095141c0366f353b142bb932cc
Hansmu <hans.miikael@gmail.com>
2016-07-18 15:50:43 +0300

    Finished application.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import Async from './middlewares/async';
 import App from './components/app';
 import reducers from './reducers';
-
+// Tells Redux that whenever an action gets dispatched, make sure it flows through this middleware.
+// If we'd have many middleware, we'd add them to the applyMiddleware with commas.
 const createStoreWithMiddleware = applyMiddleware(Async)(createStore);
 
 ReactDOM.render(
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -6,7 +6,7 @@ export default function({ dispatch }) {
     return next => action => { //From this file we're returning a function, when ran it's going to return another function. When that has run it's going to call another action.
         //Just the way the signature is.
         //If the action does not have a payload or it doesn't have a then property, then send it on.
-        if (!action.payload || !action.payload.then) {
+        if (!action.payload || !action.payload.then) { // Then check to make sure we don't pass an undefined function down below.
             return next(action); //Send to the next middleware in the stack, if there are none, send it to reducers.
             //Always call next or the action just stops.
         }
@@ -23,5 +23,7 @@ export default function({ dispatch }) {
                 }
             );
 
+        //A random example of using middleware - getting a strange response back from a specific API and then transforming into a more processable form.
+        //Catch the promise from that specific API and then process it.
     };
 }
