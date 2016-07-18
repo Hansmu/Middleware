4fecdad73cb34a16e467a21ae1483e5662244481
Hansmu <hans.miikael@gmail.com>
2016-07-18 15:45:52 +0300

    Finished middleware.

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -5,8 +5,23 @@
 export default function({ dispatch }) {
     return next => action => { //From this file we're returning a function, when ran it's going to return another function. When that has run it's going to call another action.
         //Just the way the signature is.
-        console.log(action);
+        //If the action does not have a payload or it doesn't have a then property, then send it on.
+        if (!action.payload || !action.payload.then) {
+            return next(action); //Send to the next middleware in the stack, if there are none, send it to reducers.
+            //Always call next or the action just stops.
+        }
 
-        next(action);//Send to the next middleware in the stack, if there are none, send it to reducers.
+        //Make sure the action's promise resolves.
+        action.payload
+            .then(
+                function(response) {
+                    //Create a new action with the old type, but replace the payload with response.
+                    const newAction = { ...action, payload: response }
+                    dispatch(newAction); //Send the action through all of the middlewares again. Run the entire cycle again.
+                    //Use dispatch instead of next because previous middleware has an action that must've happened previously on a resolved action.
+                    //You could try to reorder, but you shouldn't. Middleware should be stand alone and should not make any assumptions about other middleware.
+                }
+            );
+
     };
 }
